de13dd127c32836d1b78267f712abb617fd01af7
yhee <yhee@trip.com>
2020-07-08 16:23:26 +0800

    update

--- common-service-a/src/main/java/com/rainlf/springcloud/action/common/service/a/BookAController.java
+++ common-service-a/src/main/java/com/rainlf/springcloud/action/common/service/a/BookAController.java
@@ -3,12 +3,14 @@ package com.rainlf.springcloud.action.common.service.a;
 import com.rainlf.springcloud.action.common.api.book.BookApi;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author : rain
  * @date : 2020/7/8 15:14
  */
 @Slf4j
+@RestController
 public class BookAController implements BookApi {
     @Override
     public String oneBook() {
--- common-service-b/src/main/java/com/rainlf/springcloud/action/common/service/b/BookBController.java
+++ common-service-b/src/main/java/com/rainlf/springcloud/action/common/service/b/BookBController.java
@@ -2,12 +2,14 @@ package com.rainlf.springcloud.action.common.service.b;
 
 import com.rainlf.springcloud.action.common.api.book.BookApi;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author : rain
  * @date : 2020/7/8 15:14
  */
 @Slf4j
+@RestController
 public class BookBController implements BookApi {
     @Override
     public String oneBook() {
